a5ffc8dafd79adaddb88f257c6561ff9d9965310
Avelot <jose.avelinosouza@gmail.com>
2022-03-21 18:21:50 -0300

    repetiçoes (loops)

---
+++ aula12-repeticoes/repeticao.js
@@ -0,0 +1,21 @@
+
+////REPETICAO NO INICIO
+// var c = 1
+// while (c<=6) {
+//    console.log('Tudo Bem?')
+//    c++ //c = c+1
+// }
+
+////REPETICAO NO FIM
+// var c = 1
+// do {
+//     console.log(`passo ${c}`)
+//     c++
+// } while (c <= 6);
+
+//REPETICAO COM CONTROLE
+console.log('vai comecar')
+for (var c = 1; c <= 10; c++) {
+    console.log (`passo ${c}`)
+}
+console.log('fim!')
